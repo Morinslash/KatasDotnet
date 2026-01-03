4f9499f92399cb36b40b7ab7d4fc76b6d021b198
Morinslash <sebastian.elf.kulinski@gmail.com>
2026-01-03 13:32:27 +0100

    LeapYear kata: add initial test and naive implementation
    Add first unit test (LeapYearShould.ReturnTrueForYear2020) and a minimal naive implementation of LeapYear.IsLeapYear that returns true for 2020 to make the first test pass. Updated solution and test project files accordingly. This is a starting point; full leap year rules will be implemented in follow-up commits following TDD.

--- LeapYear/LeapYearSrc/LeapYear.cs
+++ LeapYear/LeapYearSrc/LeapYear.cs
@@ -2,4 +2,9 @@
 
 public class LeapYear
 {
+    public bool IsLeapYear(int year)
+    {
+        // minimal, naive implementation only for first test to pass
+        return year == 2020;
+    }
 }
--- LeapYear/LeapYearTests/LeapYearShould.cs
+++ LeapYear/LeapYearTests/LeapYearShould.cs
@@ -2,4 +2,13 @@ namespace LeapYearTests;
 
 public class LeapYearShould
 {
+    // first minimal unit test according to ../README.md
+    // assert on the simplest case
+    [Fact]
+    public void ReturnTrueForYear2020()
+    {
+        var leapYear = new LeapYearSrc.LeapYear();
+        bool result = leapYear.IsLeapYear(2020);
+        Assert.True(result);
+    }
 }
